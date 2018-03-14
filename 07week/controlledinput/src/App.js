a38b96c7b05e3586a44b8110fe1b0fcb412b3476
TODD FREDERKING <toddfrederking@gmail.com>
2018-03-13 19:06:25 -0500

    control

--- 07week/controlledinput/src/App.js
+++ 07week/controlledinput/src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      inputValue: '',
+      key: [],
+    }
+  }
+
+
   render() {
+
+    const style = {
+  margin: 12,
+  };
+
+  const RaisedButtonExampleSimple = () => (
+    <div>
+      <RaisedButton label="Default" style={style} />
+      <RaisedButton label="Primary" primary={true} style={style} />
+      <RaisedButton label="Secondary" secondary={true} style={style} />
+      <RaisedButton label="Disabled" disabled={true} style={style} />
+      <br />
+      <br />
+      <RaisedButton label="Full width" fullWidth={true} />
+    </div>
+  );
+
+
+
     return (
+        <MuiThemeProvider>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,7 +44,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {RaisedButtonExampleSimple()}
+
+
+
       </div>
+      </MuiThemeProvider>
     );
   }
 }
